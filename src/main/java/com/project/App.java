d5f5a1292eb7680a4ba77ca4065bf85fdbf6b309
BartnikLukasz <bartnik.luk@gmail.com>
2020-02-29 23:12:18 +0100

    Preparing application form

---
+++ src/main/java/com/project/App.java
@@ -0,0 +1,25 @@
+package com.project;
+
+import javax.swing.*;
+
+public class App {
+    private JPanel mainPanel;
+    private JPanel menuPanel;
+    private JButton goToUserAdditionButton;
+    private JButton goToUserTableButton;
+    private JPanel addUserPanel;
+    private JTextArea firstNameTextArea;
+    private JTextArea lastNameTextArea;
+    private JTextArea loginTextArea;
+    private JTextArea passwordTextArea;
+    private JTextArea accountTypeTextArea;
+    private JTextArea emailTextArea;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JTextField loginTextField;
+    private JTextField passwordTextField;
+    private JTextField emailTextField;
+    private JComboBox accountTypeComboBox;
+    private JButton backToMenuButton;
+    private JButton addUserButton;
+}
