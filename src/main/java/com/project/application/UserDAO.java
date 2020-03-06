a123d18ceb9cbf5dc903bbb01d81a8004bba0304
BartnikLukasz <bartnik.luk@gmail.com>
2020-03-06 15:59:42 +0100

    Improved exception handling.

--- src/main/java/com/project/application/UserDAO.java
+++ src/main/java/com/project/application/UserDAO.java
@@ -5,6 +5,7 @@ import com.project.user_database_app.User;
 import org.apache.http.message.BasicNameValuePair;
 import org.aspectj.apache.bcel.classfile.annotation.NameValuePair;
 
+import javax.swing.*;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
@@ -17,6 +18,7 @@ import java.util.List;
 
 public class UserDAO {
 
+    //Creates query that gets passed to BufferedWriter
     private String getQuery(List<BasicNameValuePair> params) throws UnsupportedEncodingException
     {
         StringBuilder result = new StringBuilder();
@@ -37,6 +39,7 @@ public class UserDAO {
         return result.toString();
     }
 
+    //Sends PUT request to Spring server
 public void addUser(User user){
 
     try {
@@ -62,13 +65,16 @@ public void addUser(User user){
         bufferedWriter.close();
         outputStream.close();
         httpURLConnection.connect();
-        System.out.println(httpURLConnection.getResponseMessage() + "\nCode: " + httpURLConnection.getResponseCode() + "\nURL: " + httpURLConnection.getURL());
+        JOptionPane.showMessageDialog(null, "User added successfully.");
 
-    }catch(IOException e){
-        e.printStackTrace();
+    }catch(IOException exc){
+        JOptionPane.showMessageDialog(null, "Error while adding client.\n" + exc.getMessage());
+        exc.printStackTrace();
     }
-}
+    }
 
+
+//Sends GET request to Spring server. Path="/all", so it doesn't take any parameters and returns list of all users
 public List<User> getAllUsers(){
 
         try {
@@ -79,7 +85,7 @@ public List<User> getAllUsers(){
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            Gson g = new Gson();
+            Gson g = new Gson();//Using Gson class to create Object out of Json
             User tempUserArray[]= g.fromJson(bufferedReader.readLine(),User[].class);
             List<User> tempUserList = Arrays.asList(tempUserArray);
 
@@ -90,11 +96,15 @@ public List<User> getAllUsers(){
             return tempUserList;
 
         }catch(Exception exc){
+            JOptionPane.showMessageDialog(null,"Error while loading client info.\n" +
+                    "Check if the Spring Boot server is running.\n" + exc.getMessage());
+            System.out.println("Check if the Spring Boot server is running.\n");
             exc.printStackTrace();
         }
         return null;
     }
 
+    //Sends GET request to Spring server. Path="search", so it takes String parameter and returns List of Users, whose last names match parameter
     public List<User> searchByLastName(String lastName){
         try{
 
@@ -122,7 +132,7 @@ public List<User> getAllUsers(){
         }
         return null;
     }
-
+    //Sends DELETE request to Spring server. Path="delete", so it takes Integer parameter
     public void deleteUserById(Integer id){
         try{
             String query = "?id="+id.toString();
@@ -133,16 +143,11 @@ public List<User> getAllUsers(){
             InputStream inputStream = httpURLConnection.getInputStream();
             httpURLConnection.connect();
         }catch(IOException exc){
+            JOptionPane.showMessageDialog(null, "Error while deleting user.\n " +
+                    "Check any user is selected.\n" + exc.getMessage());
+            System.out.println("Check any user is selected.\n");
             exc.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-
-    UserDAO userDAO = new UserDAO();
-
-        System.out.println(userDAO.searchByLastName("Zenak").get(0).getFirstName());
-
     }
 
 }
